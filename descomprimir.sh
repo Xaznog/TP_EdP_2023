9e7b6bd604814d3594c974ccaf2ee765dea2af47
German Alanis <germanp.alanis@gmail.com>
2023-07-08 18:46:33 -0300

    Cambios por modificacion de enunciado

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Descarga un archivo comprimido de imágenes.
-# De debe poder indicar por argumento dos archivos (uno con las imágenes y otro con una suma de verificación).
+# Se debe poder indicar por argumento dos archivos (uno con las imágenes comprimidas y otro con una suma de verificación).
 # Si ocurrió algún error se debe informar al usuario de lo contrario se procede a descomprimir.
 
 RUTA_TRABAJO="/home/tuia/EdP/TP"
