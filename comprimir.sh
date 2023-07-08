24094891e61263acbeefcb55b7afca7c1f0f5f7f
Sergio Castells <castellssergio@gmail.com>
2023-07-08 18:52:01 -0300

    Cambios por modificacion de enunciado

--- comprimir.sh
+++ comprimir.sh
@@ -1,49 +1,55 @@
 #!/bin/bash
 
-# Una vez procesadas las imágenes se debe generar un archivo con la lista de todas las personas, un total de personas femeninas y masculinas; y luego comprimir todo en un solo archivo.
-# Entiéndase por persona femenina aquella cuya ultima letra del primer nombre es una letra “a”.
-# El archivo generado debe poder accederse fuera del contenedor.
+# Finalmente, luego de procesadas las imágenes, se debe:
+# – generar un archivo con la lista de nombres de todas las imágenes.
+# – generar un archivo con la lista de nombres válidos.
+# – generar un archivo con el total de personas cuyo nombre finaliza con la letra a.
+# – por último, generar un archivo comprimido que incluya los archivos generados en los items anteriores y todas las imágenes.
+# El archivo comprimido debe poder accederse desde fuera del contenedor.
 
-RUTA_IMAGENES_PROC="/home/tuia/EdP/TP/imagenes_descargadas/nuevas_imagenes"
-#REG_EXP_MUJERES="^[A-Z][a-z]+a(\s[A-Z][a-z]+)?\.(.+)?$"
-#REG_EXP_PERS="^[A-Z][a-z]+(\s[A-Z][a-z]+)?\.(.+)?$"
-REG_EXP_MUJERES="^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+a(\s[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)?\.(.+)?$"
-REG_EXP_PERS="^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(\s[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)?\.(.+)?$"
+#RUTA_IMAGENES_PROC="/home/tuia/EdP/TP/imagenes_descargadas/nuevas_imagenes"
+REG_EXP_VAL_FIN_A="^[A-ZÁÉÍÓÚÑÀÈÌÒÙ][a-záéíóúñàèìòù]+a(\s[A-ZÁÉÍÓÚÑÀÈÌÒÙ][a-záéíóúñàèìòù]+)*\.(.+)?$"
+REG_EXP_VAL="^[A-ZÁÉÍÓÚÑÀÈÌÒÙ][a-záéíóúñàèìòù]+(\s[A-ZÁÉÍÓÚÑàèìòù][a-záéíóúñàèìòù]+)*\.(.+)?$"
 RUTA_COMPRIMIDO="/home/tuia/EdP/TP"
 RUTA_IMAGENES="/home/tuia/EdP/TP/imagenes_descargadas"
 
-chmod +rwx $RUTA_IMAGENES_PROC
+#chmod +rwx $RUTA_IMAGENES_PROC
 
-if [ -e $RUTA_IMAGENES_PROC ]
+if [ -e $RUTA_IMAGENES ]
 then
-	cd $RUTA_IMAGENES_PROC
-	if [ -e lista_nombres.txt ]
+	cd $RUTA_IMAGENES
+	if [ -e lista_nombres_imagenes.txt ]
 	then
-		rm lista_nombres.txt
+		rm lista_nombres_imagenes.txt
 	fi
-	if [ "$(ls -A $RUTA_IMAGENES)" ]
+	if [ -e lista_nombres_val.txt ]
 	then
-		echo "Creando lista de nombres..."
-		echo "Los nombres de las personas en las imágenes son:" > lista_nombres.txt
-	        for ARCHIVO in $RUTA_IMAGENES_PROC/*
+		rm lista_nombres_val.txt
+	fi
+	if [ -e cant_nombres_val_fin_a.txt ]
+	then
+		rm cant_nombres_val_fin_a.txt
+	fi
+	if [ "$(ls -A $RUTA_IMAGENES)" ]	# Evalúa si la carpeta tiene contenido
+	then
+		echo "Creando listas con detalles de nombres..."
+		LISTA_NOMBRES=$(ls)
+		echo -e "Los nombres de todas las imágenes son:\n$LISTA_NOMBRES" > lista_nombres_imagenes.txt	# El -e habilita el uso de escapes con \
+		echo "Los nombres válidos son:" > lista_nombres_val.txt
+		for ARCHIVO in $RUTA_IMAGENES/*
 		do
         		AUX_ARCHIVO=$(basename "$ARCHIVO")
-			if [[ $AUX_ARCHIVO =~ $REG_EXP_PERS ]]
+			if [[ $AUX_ARCHIVO =~ $REG_EXP_VAL ]]
 			then
                 		#NOMBRE=$(basename "$AUX_ARCHIVO" .JPG)
                 		#echo $NOMBRE >> lista_nombres.txt
-                		echo ${AUX_ARCHIVO%.*} >> lista_nombres.txt # ${} se usa para strings, el "%" elimina patrones, el "." es un punto y el "*" es cualquier cosa. Todo esto hace lo mismo que las dos líneas anteriores
+                		echo ${AUX_ARCHIVO%.*} >> lista_nombres_val.txt # ${} se usa para strings, el "%" elimina patrones, el "." es un punto y el "*" es cualquier cosa. Todo esto hace lo mismo que las dos líneas anteriores
             		fi
         	done
-		echo "Calculando cantidad de imágenes de hombres y de mujeres..."
-		CANT_PERSONAS=$(ls | egrep $REG_EXP_PERS | wc -l)	# Cuenta la cantidad de hombres y mujeres
-		CANT_MUJERES=$(ls | egrep $REG_EXP_MUJERES | wc -l)	# Cuenta la cantidad de mujeres
-		CANT_HOMBRES=$(($CANT_PERSONAS-$CANT_MUJERES))		# Calcula la cantidad de hombres
-		echo "La cantidad de personas femeninas es de: $CANT_MUJERES" >> lista_nombres.txt
-		echo "La cantidad de personas masculinas es de: $CANT_HOMBRES" >> lista_nombres.txt
+		CANT_VAL_FIN_A=$(ls | egrep $REG_EXP_VAL_FIN_A | wc -l)	# Cuenta la cantidad de nombres válidos que terminen en "a"
+		echo "La cantidad de nombres válidos que terminan en 'a' es de: $CANT_VAL_FIN_A" > cant_nombres_val_fin_a.txt
 		echo "Comprimiendo imágenes..."
 		zip -r $RUTA_COMPRIMIDO/imagenes_comprimidas.zip ./			# Esto comprime
-		cat lista_nombres.txt
 		rm -r $RUTA_IMAGENES
 		echo "Finalizada la compresión de imágenes."
 		exit 0
