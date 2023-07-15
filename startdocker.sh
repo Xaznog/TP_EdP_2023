01f469791578fc82a89069a0004d6bf7626b03a3
Sergio Castells <castellssergio@gmail.com>
2023-07-15 18:42:27 -0300

    Agregado de comentarios y detalles finales

--- startdocker.sh
+++ startdocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Construcción de la imagen a partir del Dockerfile
 docker build --tag img_tp_edp_tuia:1.0 .
 
+# Ejecución del contenedor y mapeo de cvolúmen para carpeta de salida
 docker run -v ./output:/home/salida -it --name tp_edp_tuia img_tp_edp_tuia:1.0
