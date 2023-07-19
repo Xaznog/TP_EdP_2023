9e48157e0f723e0449b9c2656df19ed9fc31bf81
Sergio Castells <castellssergio@gmail.com>
2023-07-18 22:41:11 -0300

    Incorporacion de uso de funciones en startdocker.sh

--- startdocker.sh
+++ startdocker.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
+# Inclusión de script con funciones
+source funciones.sh
+
 # Valida instalación de Docker
-EXISTE_DOCKER=$(whereis docker)
-if [[ $EXISTE_DOCKER == "docker:" ]]; then
-    echo -e "\e[31mERROR: Docker no está instalado. Instalelo y vuelva a intentar.\e[0m"
-	exit 1
-fi
+validar_instalacion "docker" "Docker"
 
-# Se verifica que haya conexión a internet
-CONT_REINTENTOS=0
-for REINTENTOS_CONEXION in {1..3}; do
-    wget --spider -q "www.google.com"
-    # --spider recorre los enlaces de la página web sin descargar ningún archivo
-    # -q hace que no salgan mensajes en la terminal a menos que ocurra algún error
+# Verifica que haya conexión a internet
+ping -c 3 "www.google.com" > /dev/null 2>&1
+#  -c xx indica el número de paquetes de ping que se enviarán
+# > /dev/null se utiliza para redirigir la salida estándar y que no se muestre por consola
+# 2>&1 se utiliza para redirigir la salida de error estándar a la misma ubicación que la salida estándar
 
-    # Si hay conexión a internet se procede a la creación de la imagen y posterior ejecución del contenedor
-    if [ $? -eq 0 ]; then
+# Si hay conexión a internet se procede a la creación de la imagen y posterior ejecución del contenedor
+if [ $? -eq 0 ]; then
 
-        # Construcción de la imagen a partir del Dockerfile
-        docker build --tag img_tp_edp_tuia:1.0 .
+    # Construcción de la imagen a partir del Dockerfile
+    docker build --tag img_tp_edp_tuia:1.0 .
 
-        # Ejecución del contenedor y mapeo de cvolúmen para carpeta de salida
-        docker run -v ./output:/home/salida -it --name tp_edp_tuia img_tp_edp_tuia:1.0
-        break 
-    else
-        CONT_REINTENTOS=$(($CONT_REINTENTOS + 1))
-        sleep 1
-    fi
-done
-
-if (($CONT_REINTENTOS == 3)); then
+    # Ejecución del contenedor y mapeo de cvolúmen para carpeta de salida
+    docker run -v ./output:/home/salida -it --name tp_edp_tuia img_tp_edp_tuia:1.0
+else
     echo -e "\e[31mERROR: Compruebe la conexión a internet y vuelva a intentarlo.\e[0m"
     exit 2
 fi
+
 exit 0
